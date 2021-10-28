2a3851466a01321a6df60f73132b36c13539f669
chou1111218 <91250459+chou1111218@users.noreply.github.com>
2021-10-28 17:44:34 +0800

    Add files via upload

---
+++ week07/week07-2.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+    int n=3;
+    if(n>0){
+        printf("n大於0喔\n");
+
+    }
+    while(n>0){
+        printf("while(判斷>0)有近來喔\n");
+    }
+}
---
+++ week07/week07-3.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+
+    int n=3;
+    while(n>0){
+        printf("while(判斷n>0)有近來喔,n:%d\n",n);
+
+            n--;
+    }
+        printf("while(判斷>0)有近來喔\n");
+
+}
+
